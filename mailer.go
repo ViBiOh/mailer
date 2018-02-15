63054cd5db1143ac4101de766e7a4faba225544f
Vincent Boutour <vboutour@meilleursagents.com>
2018-02-15 09:30:45 +0100

    Using new way for starting server from httputils

--- mailer.go
+++ mailer.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"net/http"
 	"strings"
 
-	"github.com/ViBiOh/alcotest/alcotest"
 	"github.com/ViBiOh/httputils"
-	"github.com/ViBiOh/httputils/cert"
 	"github.com/ViBiOh/httputils/cors"
 	"github.com/ViBiOh/httputils/owasp"
 	"github.com/ViBiOh/mailer/healthcheck"
@@ -21,63 +17,30 @@ const (
 	mailPath        = `/mail`
 )
 
-var (
-	healthcheckHandler http.Handler
-	renderHandler      http.Handler
-)
-
-func handler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasPrefix(r.URL.Path, healthcheckPath) {
-			healthcheckHandler.ServeHTTP(w, r)
-		} else if strings.HasPrefix(r.URL.Path, mailPath) {
-			renderHandler.ServeHTTP(w, r)
-		} else {
-			httputils.NotFound(w)
-		}
-	})
-}
-
 func main() {
-	port := flag.String(`port`, `1080`, `Listen port`)
-	tls := flag.Bool(`tls`, false, `Serve TLS content`)
-
-	alcotestConfig := alcotest.Flags(``)
-	certConfig := cert.Flags(`tls`)
 	corsConfig := cors.Flags(`cors`)
 	owaspConfig := owasp.Flags(``)
-
 	mailjetConfig := mailjet.Flags(``)
 
-	flag.Parse()
+	httputils.StartMainServer(func() http.Handler {
+		mailjetApp := mailjet.NewApp(mailjetConfig)
 
-	alcotest.DoAndExit(alcotestConfig)
+		renderApp := render.NewApp()
+		renderHandler := http.StripPrefix(mailPath, renderApp.Handler())
 
-	log.Printf(`Starting server on port %s`, *port)
+		healthcheckApp := healthcheck.NewApp(mailjetApp)
+		healthcheckHandler := http.StripPrefix(healthcheckPath, healthcheckApp.Handler())
 
-	mailjetApp := mailjet.NewApp(mailjetConfig)
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, healthcheckPath) {
+				healthcheckHandler.ServeHTTP(w, r)
+			} else if strings.HasPrefix(r.URL.Path, mailPath) {
+				renderHandler.ServeHTTP(w, r)
+			} else {
+				httputils.NotFound(w)
+			}
+		})
 
-	renderApp := render.NewApp()
-	renderHandler = http.StripPrefix(mailPath, renderApp.Handler())
-
-	healthcheckApp := healthcheck.NewApp(mailjetApp)
-	healthcheckHandler = http.StripPrefix(healthcheckPath, healthcheckApp.Handler())
-
-	server := &http.Server{
-		Addr:    `:` + *port,
-		Handler: owasp.Handler(owaspConfig, cors.Handler(corsConfig, handler())),
-	}
-
-	var serveError = make(chan error)
-	go func() {
-		defer close(serveError)
-		if *tls {
-			log.Print(`Listening with TLS enabled`)
-			serveError <- cert.ListenAndServeTLS(certConfig, server)
-		} else {
-			serveError <- server.ListenAndServe()
-		}
-	}()
-
-	httputils.ServerGracefulClose(server, serveError, nil)
+		return owasp.Handler(owaspConfig, cors.Handler(corsConfig, handler))
+	})
 }
